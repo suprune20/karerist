eb22386bd8ed2902a78d947adc8afe4b91cfaeb1
Eugene Suprun <suprune20@gmail.com>
2016-09-20 18:54:28 +0300

    Сформирована структура для вывода для заказчиков

--- karerist/pits/views.py
+++ karerist/pits/views.py
@@ -14,7 +14,6 @@ class CalculateView(TemplateView):
         for demand in Demand.objects.all().order_by('dt_created'):
             date_volumes = demand.date_volumes()
             pits = demand.pits_available_all()
-            print "\n", demand
             for dv in date_volumes:
                 # dict(date=..., volume=...)
                 for pit in pits:
@@ -36,8 +35,46 @@ class CalculateView(TemplateView):
                             pitmaterial=pit['pitmaterial'],
                             volume=this_volume,
                         )
-                dv['satisfied'] = dv['volume'] <= 0
-                print "\n", dv
+
+        outc = []
+        # Вывод для заказчика
+        # По каждой demand
+        # pits:     [ dict(pit=pit, pitmaterial=pitmaterial, distance=distance)
+        # ]
+        # dates:[ date: дата,
+        #         volumes: [vol_pit1, vol_pit2 ...]
+        #         total: сумма в карьерах
+        #         need: потребность на дату
+        #         satisfied: total == need
+        # [
+        for demand in Demand.objects.all().order_by('dt_created'):
+            demandc = dict(demand=demand)
+            pits = demand.pits_available_all()
+            demandc['pits'] = pits
+            demandc['dates'] = []
+            date_volumes = demand.date_volumes()
+            for dv in date_volumes:
+                date = dict(
+                    date=dv['date'],
+                    total=0,
+                    need=dv['volume']
+                )
+                volumes = []
+                for pit in pits:
+                    try:
+                        vol_in_pit = DemandResult.objects.get(
+                            demand=demand,
+                            date=dv['date'],
+                            pitmaterial=pit['pitmaterial'],
+                        ).volume
+                    except DemandResult.DoesNotExist:
+                        vol_in_pit = 0
+                    date['total'] += vol_in_pit
+                    volumes.append(vol_in_pit)
+                date['volumes'] = volumes
+                date['satisfied'] = date['total'] >= date['need']
+                demandc['dates'].append(date)
+            outc.append(demandc)
 
         return super(CalculateView, self).render_to_response(context)
 
